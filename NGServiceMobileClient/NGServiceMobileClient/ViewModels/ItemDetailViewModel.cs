dc625bbf403589a0a4683ed8cd5929559a9d60c9
vnebo <volodymyr.neborachko@zealicsolutions.com>
2019-03-09 22:55:37 +0100

    Initial commit

---
+++ NGServiceMobileClient/NGServiceMobileClient/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NGServiceMobileClient.Models;
+
+namespace NGServiceMobileClient.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
